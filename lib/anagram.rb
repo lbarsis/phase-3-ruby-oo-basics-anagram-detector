693589d3e830c1bbf074b815016fdf20ea48612e
Levi Barsis <lbarsis@gmail.com>
2022-11-25 11:09:50 -0800

    Done with assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :str
+  def initialize(str)
+    @str = str
+  end
+
+  def match(arr)
+    arr.filter {|word| word.chars.sort == @str.chars.sort}
+  end
+end
+
+binding.pry
